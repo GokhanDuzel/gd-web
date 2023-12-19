30f3ef403cf2c80cb85de239f14b80d08905ff5b
GokhanDuzel <gduze020@uottawa.ca>
2023-12-19 08:19:48 -0500

    Created Responsive Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,9 @@
 import "./Header.css";
-import logo from "../logo.png";
+import NavBar from "./NavBar";
 
 const Header = () => {
   return (
-    <div>
-      <header>
-        <nav className="navigation">
-          <img src={logo} alt="Omnifood logo" className="logo" />
-          <div className="navigation-menu">
-            <ul className="main-nav">
-              <li>
-                <a href="#">Education</a>
-              </li>
-              <li>
-                <a href="#">Projects</a>
-              </li>
-              <li>
-                <a href="#">Certificates</a>
-              </li>
-              <li>
-                <a href="#">Skills & Hobbies</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <div className="hero-text-box">
-          <h1>GÖKHAN DÜZEL</h1>
-          <h3>Software Engineer</h3>
-
-          <a className="btn btn-full" href="#">
-            About Me
-          </a>
-          <a className="btn btn-ghost" href="#">
-            Get Started
-          </a>
-        </div>
-      </header>
-    </div>
+    <NavBar />
   );
 };
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,77 @@
+import "./NavBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+
+import { useState } from "react";
+
+const NavBar = () => {
+  const [navExpanded, setNavExpanded] = useState(false);
+
+  return (
+    <div className="container">
+      <nav className="nav">
+        <div className="logo">
+          <a href="#">GD</a>
+        </div>
+        <ul className="nav-menu">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <a className="action-btn" href="#">
+          Get Started
+        </a>
+        <div className="toggle-btn">
+          {navExpanded ? (
+            <FontAwesomeIcon
+              icon={faXmark}
+              onClick={() => {
+                setNavExpanded(!navExpanded);
+              }}
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faBars}
+              onClick={() => {
+                setNavExpanded(!navExpanded);
+              }}
+            />
+          )}
+        </div>
+      </nav>
+
+      {navExpanded && (
+        <nav className="nav-dropdown">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Project</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a className="action-btn" href="#">
+              Get Started
+            </a>
+          </li>
+        </nav>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
